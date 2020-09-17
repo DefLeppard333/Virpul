070cbee259bd9e3e0f4fc06d08e6431e6dd4ca4d
Igor Shpykuliak <defleppard333@gmail.com>
2020-09-17 16:48:29 +0300

    v1.2.1

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-   $('.icon-menu').click(function (event) {
-      $('.icon-menu,.menu__body,.top-header__logo').toggleClass('_active');
+   $('.icon-menu,.menu-page__burger').click(function (event) {
+      $('.icon-menu,.menu__body,.top-header__logo,.menu-page__burger,.menu-page__body').toggleClass('_active');
       $('body').toggleClass('_lock');
    });
 });
