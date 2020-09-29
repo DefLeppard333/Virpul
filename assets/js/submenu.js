711d66d2394eb5119567e3f882a0075db865266d
Igor Shpykuliak <defleppard333@gmail.com>
2020-09-29 23:03:52 +0300

    slider

--- assets/js/submenu.js
+++ assets/js/submenu.js
@@ -1,13 +1,13 @@
 let menuParents = document.querySelectorAll('.menu-page__parent');
 
 for (let index = 0; index < menuParents.length; index++) {
-   const menuParent = menuParents[index];
-   menuParent.addEventListener("mouseenter", function (e) {
-      menuParent.classList.add('_active');
-   });
-   menuParent.addEventListener("mouseleave", function (e) {
-      menuParent.classList.remove('_active');
-   });
+    const menuParent = menuParents[index];
+    menuParent.addEventListener("mouseenter", function (e) {
+        menuParent.classList.add('_active');
+    });
+    menuParent.addEventListener("mouseleave", function (e) {
+        menuParent.classList.remove('_active');
+    });
 }
 
 
@@ -15,7 +15,8 @@ let menuPageBurger = document.querySelector('.menu-page__burger');
 let menuPageBody = document.querySelector('.menu-page__body');
 
 menuPageBurger.addEventListener("click", function (e) {
-   menuPageBurger.classList.toggle('_active');
-   menuPageBody.classList.toggle('_active');
+    menuPageBurger.classList.toggle('_active');
+    menuPageBody.classList.toggle('_active');
 });
+
 
