587a3765b96aeaf86338701dab2840bccb2c0af0
Igor Shpykuliak <defleppard333@gmail.com>
2020-10-08 16:41:13 +0300

    submenu 992

--- assets/js/submenu.js
+++ assets/js/submenu.js
@@ -1,22 +1,32 @@
-let menuParents = document.querySelectorAll('.menu-page__parent');
+let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent);
+if (isMobile) {
+   let menuParents = document.querySelectorAll('.menu-page__parent a');
+   for (let index = 0; index < menuParents.length; index++) {
+      const menuParent = menuParents[index];
+      menuParent.addEventListener("click", function (e) {
+         menuParent.parentElement.classList.toggle('_active');
+         e.preventDefault();
 
-for (let index = 0; index < menuParents.length; index++) {
-    const menuParent = menuParents[index];
-    menuParent.addEventListener("mouseenter", function (e) {
-        menuParent.classList.add('_active');
-    });
-    menuParent.addEventListener("mouseleave", function (e) {
-        menuParent.classList.remove('_active');
-    });
+      });
+   }
+
+} else {
+   let menuParents = document.querySelectorAll('.menu-page__parent');
+
+   for (let index = 0; index < menuParents.length; index++) {
+      const menuParent = menuParents[index];
+      menuParent.addEventListener("mouseenter", function (e) {
+         menuParent.classList.add('_active');
+      });
+      menuParent.addEventListener("mouseleave", function (e) {
+         menuParent.classList.remove('_active');
+      });
+   }
 }
-
-
 let menuPageBurger = document.querySelector('.menu-page__burger');
 let menuPageBody = document.querySelector('.menu-page__body');
 
 menuPageBurger.addEventListener("click", function (e) {
-    menuPageBurger.classList.toggle('_active');
-    menuPageBody.classList.toggle('_active');
+   menuPageBurger.classList.toggle('_active');
+   menuPageBody.classList.toggle('_active');
 });
-
-
