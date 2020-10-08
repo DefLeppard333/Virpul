bcf2b3b3127514d5dbbf61911eda5a7b5228bbbe
Igor Shpykuliak <defleppard333@gmail.com>
2020-10-08 16:42:23 +0300

    submenu 992

---
+++ assets/js/upbutton.js
@@ -0,0 +1,19 @@
+
+
+
+
+let smoothJumpUp = function () {
+   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      window.scrollBy(0, -50);
+      setTimeout(smoothJumpUp, 20);
+   }
+}
+
+window.onscroll = function () {
+   let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+   if (scrolled > 50) {
+      document.getElementById('upbutton').style.display = 'block';
+   } else {
+      document.getElementById('upbutton').style.display = 'none';
+   }
+}
